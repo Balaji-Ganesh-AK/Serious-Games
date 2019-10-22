dec9009240af27c240c0d846ca445f17f445f735
Joseph Newman <paradoxbuilder@gmail.com>
2019-10-22 09:17:08 -0600

    Implemented a music player object (next up: menu)

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour
+{
+    AudioClip[] songs;
+
+    AudioSource source;
+
+    const string SONG_DIRECTORY = "Songs";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        songs = Resources.LoadAll<AudioClip>(SONG_DIRECTORY);
+        if (songs.Length == 0) {
+            Debug.LogWarning($"No clips were found in {SONG_DIRECTORY}!");
+            return;
+        }
+        ChangeSong(songs[0]);
+    }
+
+    public void ChangeSong(AudioClip nextClip)
+    {
+        source.Stop();
+        source.clip = nextClip;
+        source.Play();
+    }
+
+    public void ToggleMute()
+    {
+        source.mute = !source.mute;
+    }
+
+    public void SetVolume(float volume)
+    {
+        source.volume = volume;
+    }
+}
