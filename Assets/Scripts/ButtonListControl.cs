8549d3b0f7dd9b19467274fcd949bf10400bbabe
Joseph Newman <paradoxbuilder@gmail.com>
2019-10-22 12:54:02 -0600

    Added button managers for the song select list

---
+++ Assets/Scripts/ButtonListControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class ButtonListControl : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+    public MusicPlayer player;
+
+    void Start()
+    {
+        var songs = Resources.LoadAll<AudioClip>(MusicPlayer.SONG_DIRECTORY);
+        int index = 0;
+        foreach (var song in songs)
+        {
+            GameObject button = Instantiate(buttonTemplate);
+            button.SetActive(true);
+            ButtonListButton blbc = button.GetComponent<ButtonListButton>();
+            blbc.SetText(song.name);
+            blbc.index = index++;
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+        }
+    }
+
+    public void ButtonClicked(MonoBehaviour sender)
+    {
+        player.ChangeSong((sender as ButtonListButton).index);
+    }
+}
