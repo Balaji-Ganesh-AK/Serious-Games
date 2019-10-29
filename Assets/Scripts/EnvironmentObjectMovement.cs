c6b647812f57a25f94b64e940b0a3a15f9c42f9a
Joseph Newman <paradoxbuilder@gmail.com>
2019-10-29 11:55:10 -0600

    Added component to make objects move in the world

---
+++ Assets/Scripts/EnvironmentObjectMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Bobbing : MonoBehaviour
+{
+    private float originalY;
+
+    //public Transform bobber;
+
+    public float BobbingStrength = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(
+            transform.position.x,
+            originalY + (float)System.Math.Sin(Time.realtimeSinceStartup) * BobbingStrength,
+            transform.position.z
+        );
+    }
+}
