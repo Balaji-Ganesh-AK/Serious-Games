cd52839946c12e5b05a45fda26825e0fa86b44c7
Joseph Newman <paradoxbuilder@gmail.com>
2019-10-29 13:42:09 -0600

    Work on rotation

--- Assets/Scripts/EnvironmentObjectMovement.cs
+++ Assets/Scripts/EnvironmentObjectMovement.cs
@@ -2,6 +2,8 @@
 
 public class EnvironmentObjectMovement : MonoBehaviour
 {
+    
+
     private Vector3 originalPosition;
 
     public float BobbingStrength = 1f;
@@ -17,17 +19,25 @@ public class EnvironmentObjectMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float deltaTime = Time.deltaTime;
+
+
+        var startPos = transform.position;
+        
         transform.position = new Vector3(
             originalPosition.x + (CirclingRadius > 0 ? Mathf.Cos(Time.realtimeSinceStartup / CirclingRadius) * CirclingRadius : originalPosition.x),
             originalPosition.y + Mathf.Sin(Time.realtimeSinceStartup) * BobbingStrength,
             originalPosition.z + (CirclingRadius > 0 ? Mathf.Sin(Time.realtimeSinceStartup / CirclingRadius) * CirclingRadius : originalPosition.z)
         );
 
+        var deltaPos = transform.position - startPos;
+        deltaPos /= deltaTime;
+        var omega = deltaPos / CirclingRadius;
+        var dTheta = omega * deltaTime;
         //float angle = Mathf.Acos(Vector3.Dot(originalPosition, transform.position)/(originalPosition.magnitude * transform.position.magnitude));
         //float angle = Mathf.Asin(transform.position.z);
         Vector2 perp = new Vector2(originalPosition.z, -originalPosition.x);
         //float angle = 
-        //transform.rotation = new Quaternion(transform.rotation.x, angle, transform.rotation.z, transform.rotation.w);
-        //transform.Rotate(Vector3.up, Mathf.A(originalPosition.z, originalPosition.x));
+        transform.Rotate(Vector3.up, dTheta.x);
     }
 }
